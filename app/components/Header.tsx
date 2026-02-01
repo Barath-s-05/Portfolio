1431d0d12583a56fe1112ac30f39f94a7c6a5f76
Barath-s-05 <sanbarath0509@gmail.com>
2026-02-02 00:04:43 +0530

    Fix resume filename case

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -37,7 +37,8 @@ const Header = () => {
     { name: "Contact", href: "#contact" },
   ];
 
-  const resumeLink = "/resume.pdf"; // Link to the resume page
+  const resumeLink = "https://drive.google.com/uc?export=view&id=1BeN2P0DWN7vEl6m0HC5FVuFXf6jDw_rc";
+ // Link to the resume page
 
   return (
     <>
